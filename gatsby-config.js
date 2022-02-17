c833e9930df13fc8cee8b5a9baa3f0f7dc51eca7
davidbodey <david.bodey@protonmail.com>
2022-02-17 07:20:19 -0800

    refactored mess

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,14 +7,14 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://cara.lekoarts.de`,
-    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+    siteTitle: `SeveralPhotos`,
+    siteTitleAlt: `David Bodey Photography`,
+    siteHeadline: `David Bodey - Photos of the Pacific Northwest`,
+    siteUrl: `https://d38czca6q9wivk.cloudfront.net`,
+    siteDescription: `A personal portfolio.`,
     siteLanguage: `en`,
-    siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    siteImage: `/banner.WEBP`,
+    author: `@davidbodey`,
   },
   plugins: [
     {
@@ -25,9 +25,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Cara - @lekoarts/gatsby-theme-cara`,
-        short_name: `Cara`,
-        description: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+        name: `David Bodey - Photos of the Pacific Northwest`,
+        short_name: `SeveralPhotos`,
+        description: `A personal portfolio.`,
         start_url: `/`,
         background_color: `#141821`,
         // This will impact how browsers show your PWA/website
